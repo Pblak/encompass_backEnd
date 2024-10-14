ad96f8abdf3ac770dacb1a873a1f25a20f1a4ad9
Pblak <youceflahreche@outlook.fr>
2024-10-14 03:01:50 +0100

    :ambulance:

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,37 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Parents;
+use App\Models\Student;
+use App\Models\Teacher;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request): JsonResponse
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-
         $accountType = $request->accountType && in_array($request->accountType, ['student', 'teacher', 'parent']) ? $request->accountType : 'web';
 
         // Determine the provider based on the guard
-        $provider = config("auth.guards.{$accountType}.provider");
+//        $provider = config("auth.guards.{$accountType}.provider");
 
         // Determine the model based on the provider
-        $model = config("auth.providers.{$provider}.model");
+//        $model = config("auth.providers.{$provider}.model");
 
         // Attempt to fetch the user
-        $user = (new $model)->where('email', $request->email)
-            ->orWhere('infos->username' ,$request->username)->first();
+//        $user = (new $model)->where('email', $request->email)
+//            ->orWhere('infos->username' ,$request->username)->first();
 
-        if (!$user || !Hash::check($request->password, $user->password)) {
+        $attemptLogin = call_user_func([$this, $accountType], $request);
+
+        if ($attemptLogin->status === false) {
             return response()->json([
                 'message' => 'Invalid login details'
             ], 401);
         }
 
-        $token = $user->createToken('token')->plainTextToken;
+        $token = $attemptLogin->user->createToken('auth_token')->plainTextToken;
+        $user = $attemptLogin->user;
         $user['accountType'] = $accountType;
 
         return response()->json([
@@ -40,6 +43,83 @@ class AuthController extends Controller
             'user' => $user
         ]);
     }
+
+    public function student(Request $request): object
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+        $user  = Student::where('infos->username', $request->username)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return (object)[
+                'status' => false,
+            ];
+        }else{
+            return (object)[
+                'status' => true,
+                'user' => $user
+            ];
+        }
+    }
+
+    public function web(Request $request): object
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return (object)[
+                'status' => false,
+            ];
+        }else{
+            return (object)[
+                'status' => true,
+                'user' => $user
+            ];
+        }
+    }
+    public function parent(Request $request): object
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        $user = Parents::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return (object)[
+                'status' => false,
+            ];
+        }else{
+            return (object)[
+                'status' => true,
+                'user' => $user
+            ];
+        }
+    }
+
+    public function teacher(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        $user = Teacher::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return (object)[
+                'status' => false,
+            ];
+        }else{
+            return (object)[
+                'status' => true,
+                'user' => $user
+            ];
+        }
+
+    }
+
 
     public function logout(Request $request)
     {
